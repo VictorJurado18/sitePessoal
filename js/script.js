757c02c78441a90e8ea8fb41df97d91ca2e70ecf
Victor Jurado <victor.nemejur@gmail.com>
2021-10-28 21:14:15 -0300

    Adc novas funções na section de projetos, adc de conteudo sobre.

--- js/script.js
+++ js/script.js
@@ -12,35 +12,56 @@ function toggleMenu(event) {
     event.currentTarget.setAttribute("aria-label", "Abrir Menu");
   }
 }
+function scrollToID(event) {
+  event.preventDefault();
+  const ID = event.currentTarget.getAttribute('href');
+  const target = document.querySelector(ID);
+  target.scrollIntoView({ behavior: "smooth" });
+  // fecha o menu
+  btnMobile.click();
+}
+
+// Seleciona links internos
+const intLinks = document.querySelectorAll('[href^="#"]');
+intLinks.forEach(link => {
+  link.addEventListener('click', scrollToID);
+});
 
 btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
+/*-----------------------------------------------------------------------------------------*/
 
 
-const listaProj = ['imagens/proj1.jpg','imagens/proj2.jpg','imagens/proj3.jpg','imagens/proj4.jpg'];
-const listaLink = ['https://codepen.io/victorjurado18/full/YzQOQYQ','https://codepen.io/victorjurado18/full/RwgMdEz','https://codepen.io/victorjurado18/full/OJgjzeP','https://codepen.io/victorjurado18/full/WNOXKvj'];
-
+const listaProj = ['imagens/proj1.jpg','imagens/proj2.jpg','imagens/proj3.jpg','imagens/proj4.jpg','imagens/proj5.jpg'];
+const listaLink = ['https://codepen.io/victorjurado18/full/YzQOQYQ','https://codepen.io/victorjurado18/full/RwgMdEz','https://codepen.io/victorjurado18/full/OJgjzeP','https://codepen.io/victorjurado18/full/WNOXKvj','https://github.com/VictorJurado18/calculadora_argamassa/blob/main/calculadora_de_material_para_argamassa.py'];
+const listaNome = ['Super Trunfo', 'AluraFlix', 'Calculadora de Média', 'Mentalista', 'Calculadora para Argamassa'];
 var numero = 0
 function exibeImagem(){
     
     document.getElementById("img-projetos").innerHTML = "<a href="+listaLink[numero]+" target='_blank'><img id='foto-projeto' src="+listaProj[numero]+"></a>";
     
 }
+function exibeNome(){
+    
+  document.getElementById("nomeDoProj").innerHTML = listaNome[numero];
+  
+}
 
 function avancar(){
-    if (numero < 3){
+    if (numero < 4){
     numero = numero + 1;
-    console.log(numero);
-    document.getElementById("img-projetos").innerHTML = "<a href="+listaLink[numero]+" target='_blank'><img id='foto-projeto' src="+listaProj[numero]+"></a>";
-    }
+    exibeImagem();
+    exibeNome();  
+  }
 }
 
 function voltar(){
     if (numero > 0){
     numero = numero - 1;
-    console.log(numero);
-    document.getElementById("img-projetos").innerHTML = "<a href="+listaLink[numero]+" target='_blank'><img id='foto-projeto' src="+listaProj[numero]+"></a>";
-    }
+    exibeImagem();
+    exibeNome();
+  }
 }
 
 exibeImagem();
+exibeNome();
